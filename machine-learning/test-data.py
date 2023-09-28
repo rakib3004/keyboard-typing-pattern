9ad3d95220ad4f8b31a781faa7ae73c0d269a7d6
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-09-28 23:14:34 +0600

    add test data evaluation

---
+++ machine-learning/test-data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Replace 'your_file.csv' with the actual path to your CSV file
+file_path = 'finaldata.csv'
+
+# Read the CSV file into a DataFrame
+dataset = pd.read_csv(file_path)
+data = dataset.drop(columns=['user'])
+
+
+# Get the column names (titles)
+column_names = data.columns
+
+# Create a dictionary to store the pairs of column names and their first values
+single_data_point = {}
+
+# Iterate through the column names
+for column_name in column_names:
+    # Get the first value from the column (iloc[0] selects the first row)
+    first_value = data[column_name].iloc[998]
+    
+    # Add the pair to the dictionary
+    single_data_point[column_name] = first_value
+
+# Print the resulting dictionary
+print(single_data_point)
