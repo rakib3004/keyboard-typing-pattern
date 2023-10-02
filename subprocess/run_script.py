cf9af7f2d77c8829181012e18d44e5af13c086ed
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-10-02 10:49:58 +0600

    connect python api with javascript

--- subprocess/run_script.py
+++ subprocess/run_script.py
@@ -1,29 +1,22 @@
 # server.py
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS  
 import subprocess
 import json
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/train', methods=['POST'])
 def run_script():
     try:
-        # Run the Python script using subprocess and capture the output
-        print('------------1-----------------')
+
         result = subprocess.check_output(['python', 'train.py'], text=True)
-        
-        # Parse the JSON result
-        print(result,'------------2-----------------')
         result_data = json.loads(result)
-        print('result-data-----',result_data)
         if 'accuracy' in result_data:
             accuracy = result_data["accuracy"]
-            print('accuracy got it ')
         else:
             accuracy = None
-            print('------------3-----------------')
-        # Return the result as JSON
-        print('accuracy')
         return jsonify({'success': True, 'result': accuracy})
     except Exception as e:
         return jsonify({'success': False, 'result': str(e)})
@@ -31,4 +24,3 @@ def run_script():
 
 if __name__ == '__main__':
     app.run(debug=False)
-    #run_script()
